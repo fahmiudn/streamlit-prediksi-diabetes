c6b5c3b313c6d897bee3c461ae54db49e27fc9a6
fahmiudn <fahmi.bgr999@gmail.com>
2024-10-21 09:26:45 +0700

    Update diabetes_streamlit.py

--- diabetes_streamlit.py
+++ diabetes_streamlit.py
@@ -36,7 +36,7 @@ with st.sidebar:
     page = option_menu(
         menu_title=None,  # required
         options=["Informasi Dataset", "Dashboard Visualisasi", "Model LSTM", "Prediksi Diabetes"],  # required
-        icons=["table", "globe", "robot", "file-lines"],  # optional
+        icons=["table", "globe", "robot", "list"],  # optional
         menu_icon="cast",  # optional
         default_index=0,  # optional
     )
